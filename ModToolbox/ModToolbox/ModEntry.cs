e5b64ec919b1de3448b9107f4ecfdc5ba68677b1
demiacle <demiacle@gmail.com>
2017-01-24 16:15:54 -0800

    First commit... testing 1, 2

---
+++ ModToolbox/ModToolbox/ModEntry.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace DemiacleSvm {
+    public class ModEntry : Mod {
+
+        //public static ModData modData;
+        public static ModEntry modEntry;
+        public static IModHelper helper;
+        // TODO Replace with helper method mod already has a saver and loader so change to use that
+        public static readonly string modDirectory = AppDomain.CurrentDomain.BaseDirectory + "\\Mods\\Demiacle_UiMod\\";
+        public const string saveFilePostfix = "_modData.xml";
+        public static Boolean isTesting = false;
+
+        public override void Entry( IModHelper helper ) {
+            ModEntry.helper = helper;
+            FieldInfo[] x = helper.Reflection.GetPrivateFields();
+            ModEntry.modEntry = this;
+            //modData = new ModData();
+
+            // Loads the correct settings on character load
+            SaveEvents.AfterLoad += loadModData;
+
+        }
+
+        internal static void Log( string log ) {
+            if( isTesting ) {
+                System.Console.WriteLine( log );
+                return;
+            }
+
+            modEntry.Monitor.Log( log );
+        }
+
+        /// <summary>
+        /// Loads mod specific data
+        /// </summary>
+        private void loadModData( object sender, EventArgs e ) {
+
+            string playerName = Game1.player.name;
+
+            // File: \Mods\Demiacle_SVM\playerName_modData.xml
+            // load file 
+            if ( Game1.player.name == "ModToolbox" ) {
+                initializeMod();
+            }
+
+        }
+
+        private void initializeMod() {
+            GraphicsEvents.OnPostRenderEvent += drawTextures;
+        }
+
+        private void drawTextures( object sender, EventArgs e ) {
+            if (  Game1.activeClickableMenu is SpriteSheetFinder == false ) {
+                Game1.activeClickableMenu = new SpriteSheetFinder();
+            }
+        }
+    }
+}
---
+++ ModToolbox/ModToolbox/SpriteSheetFinder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley.Menus;
+using StardewValley;
+using Microsoft.Xna.Framework;
+
+namespace DemiacleSvm {
+    internal class SpriteSheetFinder : IClickableMenu {
+
+        private int positionOfTextureX = 0;
+        private int positionOfTextureY = 0;
+        private bool isRightClicking = false;
+        private bool isLeftClicking = false;
+
+        public override void draw( SpriteBatch b ) {
+
+            // Clear screen
+            Game1.spriteBatch.Draw( Game1.staminaRect, new Rectangle( 0, 0, Game1.viewport.Width, Game1.viewport.Height ), Color.Black );
+
+            // Draw Texture Chooser
+            Game1.spriteBatch.Draw( Game1.mouseCursors, Vector2.Zero, new Rectangle( positionOfTextureX, positionOfTextureY, Game1.mouseCursors.Width, Game1.mouseCursors.Height ), Color.White, 0, Vector2.Zero, Game1.pixelZoom, SpriteEffects.None, 1 );
+
+            // Redraw mouse
+            Game1.spriteBatch.Draw( Game1.mouseCursors, new Vector2( ( float ) Game1.getOldMouseX(), ( float ) Game1.getOldMouseY() ), new Rectangle?( Game1.getSourceRectForStandardTileSheet( Game1.mouseCursors, Game1.options.gamepadControls ? 44 : 0, 16, 16 ) ), Color.White, 0f, Vector2.Zero, ( float ) Game1.pixelZoom + Game1.dialogueButtonScale / 150f, SpriteEffects.None, 1f );
+            ;
+        }
+
+        public override void receiveRightClick( int x, int y, bool playSound = true ) {
+            isRightClicking = true;
+        }
+
+        public override void receiveLeftClick( int x, int y, bool playSound = true ) {
+            base.receiveLeftClick( x, y, playSound );
+            isLeftClicking = true;
+        }
+
+        public override void releaseLeftClick( int x, int y ) {
+            base.releaseLeftClick( x, y );
+        }
+
+
+
+    }
+}
