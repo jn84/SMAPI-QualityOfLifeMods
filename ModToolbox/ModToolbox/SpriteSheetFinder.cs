4b335fc7fd0815fcd8a17a293a331cb167ac0c30
demiacle <demiacle@gmail.com>
2017-01-26 14:47:16 -0800

    Mostly working, just need to implement the string to copy

--- ModToolbox/ModToolbox/SpriteSheetFinder.cs
+++ ModToolbox/ModToolbox/SpriteSheetFinder.cs
@@ -7,6 +7,8 @@ using StardewModdingAPI.Events;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 using System.IO;
+using Microsoft.Xna.Framework.Content;
+using System.Threading;
 
 namespace ModToolbox {
     internal class SpriteSheetFinder : IClickableMenu {
@@ -16,120 +18,66 @@ namespace ModToolbox {
         private bool isRightClicking = false;
         private bool isLeftClicking = false;
         private List<Texture2D> allTextures = new List<Texture2D>();
-        private List<string> textureLocations = new List<string>();
+        private List<string> contentLocation = new List<string>();
         private int currentTextureIndex = 0;
 
         private int scale = 4;
 
         private Texture2D currentTexture = Game1.mouseCursors;
 
+        private int positionToCropX;
+        private int positionToCropY;
+        private int widthOfCrop;
+        private int heightOfCrop;
+        private bool isShifting = false;
+        private bool isSnappingToTileSize = false;
+
+        private int rightClickStartPositionX;
+        private int rightClickStartPositionY;
+        private int rightClickDeltaY;
+        private int rightClickDeltaX;
+        private TextBox boxToCopyFrom = new TextBox( Game1.content.Load<Texture2D>( "LooseSprites\\textBox" ), ( Texture2D ) null, Game1.smallFont, Game1.textColor );
+
         public SpriteSheetFinder(  ) {
             ControlEvents.MouseChanged += releaseRightClick;
             string test = Game1.content.RootDirectory;
 
-            string exeDirectory = AppDomain.
+            string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
-            bool ignore = true;
-            if ( ignore == false ) { 
-            textureLocations.Add( Path.Combine( "Minigames", "Clouds" ) );
-            textureLocations.Add( "LooseSprites\\font_colored" );
-            textureLocations.Add( "LooseSprites\\font_bold" );
-            textureLocations.Add( "LooseSprites\\hoverBox" );
-            textureLocations.Add( "TileSheets\\Projectiles" );
-            textureLocations.Add( "TileSheets\\weapons" );
-            textureLocations.Add( "Animals\\BabySheep" );
-            textureLocations.Add( "Animals\\BabyGoat" );
-            textureLocations.Add( "Animals\\BabyPig" );
-            textureLocations.Add( "Animals\\BabyWhiteBlackCow" );
-            textureLocations.Add( "Animals\\Cow" );
-            textureLocations.Add( "Animals\\Duck" );
-            textureLocations.Add( "Animals\\BabyDuck" );
-            textureLocations.Add( "LooseSprites\\Bat" );
-            textureLocations.Add( "LooseSprites\\Cursors" );
-            textureLocations.Add( "Animals\\Chicken" );
-            textureLocations.Add( "Animals\\White Chicken" ); // questionable
-            //textureLocations.Add( "Buildings\\" ); need to find names for all buildings 
-            textureLocations.Add( "LooseSprites\\robinAtWork" );
-            textureLocations.Add( "Animals\\cat" );
-            textureLocations.Add( "Animals\\dog" );
-            textureLocations.Add( "Animals\\horse" );
-            textureLocations.Add( "Characters\\Junimo" );
-            textureLocations.Add( "Animals\\Dinosaur" );
-            textureLocations.Add( "Animals\\BabyRabbit" );
-            textureLocations.Add( "Animals\\BabyBrownChicken" );
-            textureLocations.Add( "Animals\\BabyBrownChicken" );
-            textureLocations.Add( "Animals\\BabyWhiteChicken" );
-            textureLocations.Add( "LooseSprites\\textBox" );
-            textureLocations.Add( "Buildings\\houses" );
-            textureLocations.Add( "Characters\\Farmer\\farmer_girl_base" );
-            textureLocations.Add( "Characters\\Farmer\\farmer_base" );
-            textureLocations.Add( "Characters\\Farmer\\hairstyles" );
-            textureLocations.Add( "Characters\\Farmer\\shirts" );
-            textureLocations.Add( "Characters\\Farmer\\hats" );
-            textureLocations.Add( "Characters\\Farmer\\accessories" );
+            getAllFilesInThisDirectory( exeDirectory + "\\Content" );
 
-            textureLocations.Add( "Characters\\Farmer\\shoeColors" );
-            textureLocations.Add( "Characters\\Farmer\\skinColors" );
-            textureLocations.Add( "Portraits\\Gil" );
-            textureLocations.Add( "Maps\\CommunityCenter_Ruins" );
-            textureLocations.Add( "Characters\\Krobus" );
-            textureLocations.Add( "LooseSprites\\Billboard" );
-            textureLocations.Add( "LooseSprites\\buildingPlacementTiles" );
-            textureLocations.Add( "LooseSprites\\chatBox" );
-            textureLocations.Add( "Characters\\farmergirl" );
-            textureLocations.Add( "LooseSprites\\farmMap" );
-            textureLocations.Add( "Data\\Fish" );
-            textureLocations.Add( "Characters\\Clint" );
-            //textureLocations.Add();
-            foreach( var location in textureLocations ) {
-                allTextures.Add( Game1.content.Load<Texture2D>( location) );
+            for( int i = contentLocation.Count - 1; i >= 0; i-- ) {
+                try {
+                    allTextures.Add( Game1.content.Load<Texture2D>( contentLocation[ i ] ) );
+                } catch ( ContentLoadException e ) {
+                    contentLocation.RemoveAt( i );
+                }
             }
 
-            }
+            allTextures.Reverse();
 
+            boxToCopyFrom.Width = 300;
+            boxToCopyFrom.Height = 200;
 
-            textureLocations.Add( "Characters" );
-            //textureLocations.Add();
-            foreach( var location in textureLocations ) {
-                allTextures.Add( Game1.content.Load<Texture2D>( location ) );
-            }
-
-            allTextures.Add( Game1.objectSpriteSheet );
-            allTextures.Add( Game1.toolSpriteSheet );
-            allTextures.Add( Game1.cropSpriteSheet );
-            //allTextures.Add( Game1.mailboxTexture );
-            allTextures.Add( Game1.emoteSpriteSheet );
-            allTextures.Add( Game1.debrisSpriteSheet );
-            //allTextures.Add( Game1.toolIconBox );
-            //allTextures.Add( Game1.rainTexture );
-            allTextures.Add( Game1.bigCraftableSpriteSheet );
-            //allTextures.Add( Game1.swordSwipe );
-            //allTextures.Add( Game1.swordSwipeDark );
-            allTextures.Add( Game1.buffsIcons );
-            //allTextures.Add( Game1.daybg );
-            //allTextures.Add( Game1.nightbg );
-            //allTextures.Add( Game1.logoScreenTexture );
-            //allTextures.Add( Game1.tvStationTexture );
-            //allTextures.Add( Game1.cloud );
-            allTextures.Add( Game1.menuTexture );
-            //allTextures.Add( Game1.lantern );
-            //allTextures.Add( Game1.windowLight );
-            //allTextures.Add( Game1.sconceLight );
-            //allTextures.Add( Game1.cauldronLight );
-            //allTextures.Add( Game1.shadowTexture );
-            allTextures.Add( Game1.mouseCursors );
-            //allTextures.Add( Game1.indoorWindowLight );
-            allTextures.Add( Game1.animations );
-            //allTextures.Add( Game1.titleScreenBG );
-            //allTextures.Add( Game1.logo );
-            //allTextures.Add( Game1.fadeToBlackRect );
-            //allTextures.Add( Game1.staminaRect );
-            //allTextures.Add( Game1.currentCoopTexture );
-            //allTextures.Add( Game1.currentBarnTexture );
-            //allTextures.Add( Game1.currentHouseTexture );
-            //allTextures.Add( Game1.greenhouseTexture );
-            //allTextures.Add( Game1.littleEffect );
     }
+
+        private void getAllFilesInThisDirectory( string exeDirectory, string pathName = "" ) {
+            var directoryInfo = new DirectoryInfo( exeDirectory );
+
+            FileInfo[] xnbFiles = directoryInfo.GetFiles( "*.xnb" );
+
+            foreach( var item in xnbFiles ) {
+                contentLocation.Add( pathName + item.Name.Split( '.' )[0] );
+            }
+
+            // Search directory if any
+            DirectoryInfo[] directoriesInsideThisDirectory = directoryInfo.GetDirectories();
+            foreach( var item in directoriesInsideThisDirectory ) {
+                getAllFilesInThisDirectory( item.FullName, pathName + item.Name + "\\" );
+            }
+
+            
+        }
 
         private void releaseRightClick( object sender, EventArgsMouseStateChanged e ) {
             if ( e.NewState.RightButton == Microsoft.Xna.Framework.Input.ButtonState.Released ) {
@@ -142,10 +90,24 @@ namespace ModToolbox {
             // Clear screen
             Game1.spriteBatch.Draw( Game1.staminaRect, new Rectangle( 0, 0, Game1.viewport.Width, Game1.viewport.Height ), Color.Black );
 
-            // Draw Texture Chooser
+            // Draw Texture
             Texture2D currentTexture = allTextures[ currentTextureIndex ];
             Game1.spriteBatch.Draw( currentTexture, new Vector2( positionOfTextureX, positionOfTextureY ), new Rectangle( 0, 0, currentTexture.Width, currentTexture.Height ), Color.White, 0, Vector2.Zero, scale, SpriteEffects.None, 1 );
 
+            // Draw highlighted selection
+            Color color;
+            if( isShifting ) {
+                color = Color.Red;
+            } else {
+                color = Color.White;
+            }
+
+            if( isLeftClicking ) {
+                Game1.spriteBatch.Draw( Game1.staminaRect, new Rectangle( positionToCropX + positionOfTextureX - 1, positionToCropY + positionOfTextureY - 1, widthOfCrop + 1, 1 ), color );
+                Game1.spriteBatch.Draw( Game1.staminaRect, new Rectangle( positionToCropX + positionOfTextureX - 1, positionToCropY + positionOfTextureY - 1, 1, heightOfCrop + 1 ), color );
+                Game1.spriteBatch.Draw( Game1.staminaRect, new Rectangle( positionToCropX + positionOfTextureX + widthOfCrop, positionToCropY + positionOfTextureY - 1, 1, heightOfCrop + 1 ), color );
+                Game1.spriteBatch.Draw( Game1.staminaRect, new Rectangle( positionToCropX + positionOfTextureX - 1, positionToCropY + positionOfTextureY + heightOfCrop , widthOfCrop + 1, 1 ), color );
+            }
 
             int tooltipWidth = 900;
             int tooltipHeight = 100;
@@ -153,25 +115,63 @@ namespace ModToolbox {
             int tooltipPositionX = Game1.viewport.Width / 2 - tooltipWidth / 2;
             int tooltipPositionY = Game1.viewport.Height - 100;
 
-            // Draw tooltips
+            // Draw bottom tooltip
             IClickableMenu.drawTextureBox( Game1.spriteBatch, tooltipPositionX, tooltipPositionY, tooltipWidth, tooltipHeight, Color.White );
             Game1.spriteBatch.DrawString( Game1.smallFont, "Drag Left Mouse : Select Texture | Drag Right Mouse : Reposition texture", new Vector2( tooltipPositionX + 20, tooltipPositionY + 24 ), Color.Black );
-            Game1.spriteBatch.DrawString( Game1.smallFont, "Up Arrow : Increase  Scale | Down Arrow : Decrease Scale", new Vector2( tooltipPositionX + 100, tooltipPositionY + 54 ), Color.Black );
+            Game1.spriteBatch.DrawString( Game1.smallFont, "1, 2, 3, 4 : Rescale texture | up, down, left, right : Rescale selection", new Vector2( tooltipPositionX + 100, tooltipPositionY + 54 ), Color.Black );
+            
+            // Draw top right toolotip
+            string currentTextureFileDisplay = "Current Texture: " + contentLocation[ currentTextureIndex ].Replace( @"\", @"-" );
+            
+            int tooltip2Width = (int) Game1.smallFont.MeasureString( currentTextureFileDisplay ).X + 40;
+            int tooltip2Height = 70;
+
+            int tooltip2PositionX = Game1.viewport.Width - tooltip2Width;
+            int tooltip2PositionY = 20;
+
+            IClickableMenu.drawTextureBox( Game1.spriteBatch, tooltip2PositionX, tooltip2PositionY, tooltip2Width, tooltip2Height, Color.White );
+            Game1.spriteBatch.DrawString( Game1.smallFont, currentTextureFileDisplay, new Vector2( tooltip2PositionX + 20, tooltip2PositionY + 24 ), Color.Black );
+
+            // Draw feedback
+            if( isSnappingToTileSize ) {
+                string text = "Snapping";
+                int snappingWidth = (int) Game1.smallFont.MeasureString( text ).X + 40;
+                int snappingHeight = (int) Game1.smallFont.MeasureString( text ).Y + 24;
+
+                int snappingPositionX = Game1.viewport.Width / 2 - snappingWidth / 2;
+                int snappingPositionY = Game1.viewport.Height - 140;
+                
+                IClickableMenu.drawTextureBox( Game1.spriteBatch, snappingPositionX, snappingPositionY, snappingWidth, snappingHeight, Color.White );
+                Game1.spriteBatch.DrawString( Game1.smallFont, text, new Vector2( snappingPositionX + 16, snappingPositionY + 16), Color.Black );
+            }
+
+            // Draw box to copy from
+            boxToCopyFrom.Draw( b );
 
             // Redraw mouse
             Game1.spriteBatch.Draw( Game1.mouseCursors, new Vector2( ( float ) Game1.getOldMouseX(), ( float ) Game1.getOldMouseY() ), new Rectangle?( Game1.getSourceRectForStandardTileSheet( Game1.mouseCursors, Game1.options.gamepadControls ? 44 : 0, 16, 16 ) ), Color.White, 0f, Vector2.Zero, ( float ) Game1.pixelZoom + Game1.dialogueButtonScale / 150f, SpriteEffects.None, 1f );
-            ;
         }
 
         public override void update( GameTime time ) {
             base.update( time );
             
             if( isRightClicking ) {
+
+
                 int deltaX = Game1.getMouseX() - Game1.getOldMouseX();
                 int deltaY = Game1.getMouseY() - Game1.getOldMouseY();
 
-                positionOfTextureX += deltaX;
-                positionOfTextureY += deltaY;
+                if( isSnappingToTileSize ) {
+                    rightClickDeltaX += deltaX;
+                    rightClickDeltaY += deltaY;
+
+                    positionOfTextureX = ( rightClickDeltaX / Game1.tileSize ) * 64;
+                    positionOfTextureY = ( rightClickDeltaY / Game1.tileSize ) * 64;
+                } else {
+                    positionOfTextureX += deltaX;
+                    positionOfTextureY += deltaY;
+                }
+
             }
 
         }
@@ -179,7 +179,7 @@ namespace ModToolbox {
         public override void receiveScrollWheelAction( int direction ) {
             base.receiveScrollWheelAction( direction );
 
-            if ( direction > 0 ) {
+            if ( direction < 0 ) {
                 currentTextureIndex++;
 
                 if ( currentTextureIndex > allTextures.Count - 1 ) {
@@ -187,7 +187,7 @@ namespace ModToolbox {
                 }
             }
 
-            if ( direction < 0 ) {
+            if ( direction > 0 ) {
                 currentTextureIndex--;
                 if ( currentTextureIndex < 0 ) {
                     currentTextureIndex = allTextures.Count - 1;
@@ -201,28 +201,142 @@ namespace ModToolbox {
 
         public override void receiveRightClick( int x, int y, bool playSound = true ) {
             isRightClicking = true;
+            rightClickStartPositionX = Game1.getMouseX();
+            rightClickStartPositionY = Game1.getMouseY();
         }
 
         public override void receiveLeftClick( int x, int y, bool playSound = true ) {
             base.receiveLeftClick( x, y, playSound );
             isLeftClicking = true;
+
+            // Snap to tiles
+            if( isSnappingToTileSize ) {
+                snapSelectionToTiles( ref x, ref y );
+            }
+
+            positionToCropX = x - positionOfTextureX;
+            positionToCropY = y - positionOfTextureY;
+
+        }
+
+        public override void leftClickHeld( int x, int y ) {
+            base.leftClickHeld( x, y );
+            setCropSize( x, y );
+        }
+
+        private void snapSelectionToTiles( ref int x, ref int y ) {
+
+            int xRemainder = x % Game1.tileSize;
+            int yRemainder = y % Game1.tileSize;
+
+            int amountOfTilesX = x / Game1.tileSize;
+            int amountOfTilesY = y / Game1.tileSize;
+
+            if( xRemainder > Game1.tileSize / 2 ) {
+                amountOfTilesX++;
+            }
+
+            if( yRemainder > Game1.tileSize / 2 ) {
+                amountOfTilesY++;
+            }
+
+            x = amountOfTilesX * Game1.tileSize;
+            y = amountOfTilesY * Game1.tileSize;
+
+        }
+
+        private void setCropSize( int x, int y ) {
+
+            if( x < positionToCropX || y < positionToCropY ) {
+                return;
+            }
+
+            // Snap to tiles
+            if( isSnappingToTileSize ) {
+                snapSelectionToTiles( ref x, ref y );
+            }
+
+            widthOfCrop = x - positionOfTextureX - positionToCropX;
+            heightOfCrop = y - positionOfTextureY - positionToCropY;
+
         }
 
         public override void releaseLeftClick( int x, int y ) {
             base.releaseLeftClick( x, y );
-
-            // post feedback that says text is copied to clipboard
+            setCropSize( x, y );
         }
 
         public override void receiveKeyPress( Keys key ) {
             base.receiveKeyPress( key );
 
-            if( key == Keys.Up ) {
-                scale = Math.Min( scale + 1, 4 ); 
+            if( key == Keys.D1 ) {
+                scale = 1; 
+            }
+
+            if( key == Keys.D2 ) {
+                scale = 2;
+            }
+
+            if( key == Keys.D3 ) {
+                scale = 3;
+            }
+            
+            if( key == Keys.D4 ) {
+                scale = 4;
+            }
+
+            if( key == Keys.LeftShift || key == Keys.RightShift ) {
+                isShifting = !isShifting;
+            }
+
+            if( key == Keys.Down && isShifting ) {
+                positionToCropY++;
+            }
+
+            if( key == Keys.Up && isShifting ) {
+                positionToCropY--;
+            }
+
+            if( key == Keys.Left && isShifting ) {
+                positionToCropX--;
+            }
+
+            if( key == Keys.Right && isShifting) {
+                positionToCropX++;
             }
 
             if( key == Keys.Down ) {
-                scale = Math.Max( scale - 1, 1 );
+                heightOfCrop++;
+            }
+
+            if( key == Keys.Up ) {
+                heightOfCrop--;
+            }
+
+            if( key == Keys.Left ) {
+                widthOfCrop--;
+            }
+            
+            if( key == Keys.Right ) {
+                widthOfCrop++;
+            }
+
+            if ( key == Keys.Space ) {
+                isSnappingToTileSize = !isSnappingToTileSize;
+            }
+
+            // Currently windows only
+            if( key == Keys.Enter ) {
+
+                
+
+                string stringToClipboard = "test";
+
+                // Windows only
+                var thread = new Thread( () => System.Windows.Forms.Clipboard.SetText( stringToClipboard ) );
+                thread.SetApartmentState( ApartmentState.STA ); //Set the thread to STA
+                thread.Start();
+                thread.Join(); //Wait for the thread to end
             }
 
         }
